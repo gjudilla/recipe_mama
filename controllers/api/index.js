73859d2a85936f57dcf86dd5d49d47dcb70825b0
interuptingcow <npad28@gmail.com>
2024-02-15 21:06:11 -0800

    added routes functionality

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const dashboardRoutes = require('./dashboardRoutes');
+// const pantryRoutes = require('./pantryRoutes.js')
+// const userRoutes = require('./userRoutes.js')
+
+router.use('/dashboard', dashboardRoutes);
+// router.use('/users', userRoutes)
+// router.use('/pantry', pantryRoutes)
+
+module.exports = router;
