d117becea463e20b4c84ab77ce0b715a5c0d8ba8
Jeffreydne <Jeffreydne@gmail.com>
2024-02-15 21:40:18 -0800

    added api/userROutes.js & new user signup added to this file

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// CREATE new user from sign up 
+router.post('/', async (req, res) => {
+    try {
+        const newUserData = await User.create({
+            userName: req.body.userName,
+            userPassword: req.body.userPassword,
+        });
+
+        // set up the session loggedInstatus set to true
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(newUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
