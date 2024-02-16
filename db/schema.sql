25f8145349800a3927c13e66207fcfe8e925a168
gjudilla <gjudilla@gmail.com>
2024-02-15 19:41:45 -0800

    db change

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE users_db;
 USE users_db;
 
 CREATE TABLE `accounts` (
-  `userID` int(30) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(50) NOT NULL,
-  `userPassword` varchar(50) NOT NULL,
+  `userID` INT(30) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `userName` VARCHAR(50) NOT NULL,
+  `userPassword` VARCHAR(50) NOT NULL,
 )
