972c37872eb641a4362bf38783aafbe4ef4f7d1d
Jeffreydne <Jeffreydne@gmail.com>
2024-02-19 18:46:41 -0800

    added connections to new contollers

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS blog_db;
 CREATE DATABASE blog_db;
 USE blog_db;
 
-CREATE TABLE `accounts` (
-  `userID` INT(30) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `userName` VARCHAR(50) NOT NULL,
-  `userPassword` VARCHAR(50) NOT NULL,
-)
+-- CREATE TABLE `accounts` (
+--   `userID` INT(30) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   `userName` VARCHAR(50) NOT NULL,
+--   `userPassword` VARCHAR(50) NOT NULL,
+-- )
